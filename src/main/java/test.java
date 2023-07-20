cc1f733e7d2aee05b57d0e304906ec1fc88abe9c
jerry <lgh9758@naver.com>
2023-07-20 14:37:15 +0900

    init test

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @since       2023.07.20
+ * @author      jerry
+ * @description test
+ **********************************************************************************************************************/
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
